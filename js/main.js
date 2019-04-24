982965fa0b5b8c3fb9d7ff07b139591fad8adf8d
o4eredko <evgeny.ocheredko@gmail.com>
2019-04-24 14:22:17 +0300

    Ajax form

--- js/main.js
+++ js/main.js
@@ -36,14 +36,6 @@ function validateForm(form) {
 	}
 	return true;
 }
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
 
 registration.onsubmit = postRequest;
 function postRequest(e) {
