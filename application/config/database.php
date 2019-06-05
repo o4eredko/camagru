770828a6fca482bde028c5d1c910801a2a6b9ce9
o4eredko <evgeny.ocheredko@gmail.com>
2019-06-05 17:49:18 +0300

    Updated config

--- application/config/database.php
+++ application/config/database.php
@@ -1,29 +1,13 @@
-<<<<<<< HEAD
 <?php
 
-$DB_HOST = "localhost";
-$DB_NAME = "camagru";
+$DB_HOST = "remotemysql.com";
+$DB_NAME = "EbumYmCv3K";
 $DB_CHARSET = "utf8";
 $DB_DSN = "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=$DB_CHARSET";
-$DB_USER = "root";
-$DB_PASSWORD = "36673667";
+$DB_USER = "EbumYmCv3K";
+$DB_PASSWORD = "8tdbKY8Vct";
 $DB_OPT = [
     PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES		=> false
 ];
-=======
-<?php
-
-$DB_HOST = "localhost";
-$DB_NAME = "camagru";
-$DB_CHARSET = "utf8";
-$DB_DSN = "mysql:host=$DB_HOST;dbname=$DB_NAME;charset=$DB_CHARSET";
-$DB_USER = "root";
-$DB_PASSWORD = "";
-$DB_OPT = [
-    PDO::ATTR_ERRMODE				=> PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE	=> PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES		=> false
-];
->>>>>>> 53b532fbeb2fe13f1539f0e769a12b8dc8f4e50c
